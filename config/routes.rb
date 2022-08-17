52b3ad274d0fed703b1727668034bc1ea573dab8
Xinchu Gao xinchu-gao@outlook.com <xinchu-gao@outlook.com>
2022-08-17 14:57:42 +0200

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # resources :restaurants do
+  # resources :reviews, only: [:new]
+  # end
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "restaurants", to: "restaurants#index" # see all
+  # get "restaurants/new", to: "restaurants#new" # user creates a new restaurant
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show" # see one
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
