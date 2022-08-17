c7dd1751f59b442bbe2710f858c64636a9a4006f
Xinchu Gao xinchu-gao@outlook.com <xinchu-gao@outlook.com>
2022-08-17 12:38:37 +0200

    Create model for restaurant and review

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  RANGE = (0..5)
+  validates :content, :rating, presence: true
+  validates :rating, inclusion: { in: RANGE }, numericality: { only_integer: true }
+end
